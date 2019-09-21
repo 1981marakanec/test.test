27d5542c668a8bd64174a528cd300bc6996b0ffb
marakanec <marakanec1981e30@gmail.com>
2019-09-21 16:52:30 +0300

    new text

---
+++ cofe.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Получить данные из терменала в переменную shugar
+shugar = input ('вы хотите сладкий кофе?')
+if (shugar =="1"):
+	print ('Сладкий кофе')
+elif (shugar =="2"):
+	print ("Без сахара")
+else:
+	print("Я тебя не понимать")
